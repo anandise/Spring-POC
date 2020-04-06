7a50171e76a6c029bb3d56139b531ef256459aab
Anand <a.kotrappa.ullagaddi@accenture.com>
2020-04-06 00:20:51 -0400

    Spring dependency injection

---
+++ SpringDemo/src/DependencyInjection/Triangle.java
@@ -0,0 +1,38 @@
+package DependencyInjection;
+
+public class Triangle {
+	
+	private String type;
+	private int hight;
+	
+	public int getHight() {
+		return hight;
+	}
+
+	/*public void setHight(int hight) {
+		this.hight = hight;
+	} */
+
+	public Triangle(String type) {
+		
+		this.type = type;
+	}
+	
+public Triangle(String type, int hight) {
+		
+		this.type = type;
+		this.hight = hight;
+		
+	}
+public String getType() {
+		return type;
+	}
+
+	/*public void setType(String type) {
+		this.type = type;
+	}*/
+
+public void draw() {
+	System.out.println(getType() +" Triangle drawn of hight" +getHight());
+}
+}
