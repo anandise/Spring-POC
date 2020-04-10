a3e1e4965d3f4f0544efef8fd8ea26a7ee051f0a
Anand <a.kotrappa.ullagaddi@accenture.com>
2020-04-10 01:01:27 -0400

    Spring MVC

---
+++ SpringDemo/src/JDBCTemplate/Employee.java
@@ -0,0 +1,11 @@
+package JDBCTemplate;
+
+
+	public class Employee {  
+		private int id;  
+		private String name;  
+		private float salary;  
+		//no-arg and parameterized constructors  
+		//getters and setters  
+
+}
---
+++ SpringDemo/src/SetterInjection/Address.java
@@ -0,0 +1,13 @@
+package SetterInjection;
+
+
+	public class Address {  
+		private String addressLine1,city,state,country;  
+		  
+		//getters and setters  
+		  
+		public String toString(){  
+		    return addressLine1+" "+city+" "+state+" "+country;  
+		}  
+
+}
